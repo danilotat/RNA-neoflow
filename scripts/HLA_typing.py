3f04ed43751c98162b41b4a9c13fd498e7ec3641
danilotat <36413174+danilotat@users.noreply.github.com>
2023-01-19 09:06:15 +0100

    Fixed, now for real

--- scripts/HLA_typing.py
+++ scripts/HLA_typing.py
@@ -8,8 +8,8 @@ with open(sys.argv[1], 'r') as hla_input:
         if line.split('\t')[0] in ['HLA-A', 'HLA-B', 'HLA-C']:
             # check if homo/hetero zygous
             if line.split('\t')[1] == 2:
-                HLAs.append(line.split('\t')[2].split(':')[:-1])
-                HLAs.append(line.split('\t')[5].split(':')[:-1])
+                HLAs.append(':'.join(line.split('\t')[2].split(':')[:-1]))
+                HLAs.append(':'.join(line.split('\t')[5].split(':')[:-1]))
             else:
-                HLAs.append(line.split('\t')[2].split(':')[:-1])
-print(','.join([x for x in HLAs]))
+                HLAs.append(':'.join(line.split('\t')[2].split(':')[:-1]))
+print(','.join(HLAs))
