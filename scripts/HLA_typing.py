d1d26fc3af73058308cf21a335bf503d7f1214f3
danilotat <danilotatoni@gmail.com>
2024-01-26 13:00:02 +0100

    Fixed the HLA parsing

--- scripts/HLA_typing.py
+++ scripts/HLA_typing.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3 
 
 import sys
+import pandas as pd
 
-with open(sys.argv[1], 'r') as hla_input:
+def main(hla_genotype: str):
+    """
+    Simple function to read t1k output and return HLA types
+    """
+    df = pd.read_csv(hla_genotype, sep='\t', index_col=False, names=["gene_name","num_diff_alleles","allele_1","abundance_1","quality_1","allele_2","abundance_2","quality_2","secondary_alleles"])
+    # filter to retain only HLA-A, HLA-B, HLA-C
+    df = df[df["gene_name"].isin(["HLA-A","HLA-B","HLA-C"])]
     HLAs = []
-    for line in hla_input:
-        if line.split('\t')[0] in ['HLA-A', 'HLA-B', 'HLA-C']:
-            # check if homo/hetero zygous
-            if line.split('\t')[1] == 2:
-                HLAs.append(':'.join(line.split('\t')[2].split(':')[:-1]))
-                HLAs.append(':'.join(line.split('\t')[5].split(':')[:-1]))
-            else:
-                HLAs.append(':'.join(line.split('\t')[2].split(':')[:-1]))
-print(','.join(HLAs))
+    for row in df.itertuples():
+        if row.quality_1 > 0:
+            HLAs.append(':'.join(row.allele_1.split(":")[:-1]))
+        if row.quality_2 > 0:
+            HLAs.append(':'.join(row.allele_2.split(":")[:-1]))
+    print(','.join(HLAs))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
